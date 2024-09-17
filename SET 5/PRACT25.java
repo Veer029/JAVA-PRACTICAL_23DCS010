760f0d3003291428be6ae28c8866808f16c700bb
Veer Bhalodia <163969605+Veer029@users.noreply.github.com>
2024-09-17 20:59:21 +0530

    PRACT25.java

--- SET 5/PRACT25.java
+++ SET 5/PRACT25.java
@@ -1,16 +1,16 @@
 public class PRACT25 {
-  public static void main(String[] args) {
-    try {
-      int result = divideNumbers(5, 0);
-      System.out.println("Result: " + result);
-    } catch (ArithmeticException e) {
-      System.out.println("Error: " + e.getMessage());
-    }
-  }
-  public static int divideNumbers(int dividend, int divisor) {
-    if (divisor == 0) {
-      throw new ArithmeticException("Cannot divide the given number by zero!");
-    }
-    return dividend / divisor;
-  }
+
+public static void main(String[] args) {
+try {
+
+int number = 10;
+int result = number / 0;
+System.out.println("The result is: " + result);
+} catch (ArithmeticException e) {
+
+System.out.println(" Division by zero not allowed.");
+}
+
+System.out.println("Program continues after the try-catch block.");
+}
 }
